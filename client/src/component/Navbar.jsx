79c2c5d121cc8be278440aaec5509f50f9bd8e1b
patrickstar652 <sheenpu31@gmail.com>
2025-05-20 22:55:21 +0800

    從token取得account

--- client/src/component/Navbar.jsx
+++ client/src/component/Navbar.jsx
@@ -1,6 +1,10 @@
 import { Link } from "react-router-dom";
 import { FaBook } from "react-icons/fa";
+import * as jwt_decode from "jwt-decode";
 function Navbar({ handleLogout }) {
+
+  const token = localStorage.getItem("token");
+  let user_info = jwt_decode.jwtDecode(token);
   return (
     <>
       <nav className="relative text-3xl text-white bg-blue-200 opacity-80 py-3">
@@ -13,6 +17,7 @@ function Navbar({ handleLogout }) {
           <Link>登入</Link>
           <Link onClick={handleLogout}>登出</Link>
         </div>
+        <span className="absolute right-6 top-1/2 -translate-y-1/2 flex items-center gap-2">{user_info.account}</span>
       </nav>
     </>
   );
